e7c78bfe2105395b93b8cdd3b749d383378af71c
刘海军 <lhj_terryl@foxmail.com>
2023-08-04 00:10:29 +0800

    feat: 倍速功能增加1.75x倍速

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,6 @@
 export const speed = [
   { label: '2.0x', value: 2 },
+  { label: '1.75x', value: 1.75 },
   { label: '1.5x', value: 1.5 },
   { label: '1.25x',value: 1.25 },
   { label: '1.0x', value: 1 },
